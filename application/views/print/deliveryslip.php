2b9b6b6d3f21758e863f61319d3233d142aee000
Andy Awidarto <andy@sinaptix.com>
2012-02-16 15:15:04 +0700

    add many features

--- application/views/print/deliveryslip.php
+++ application/views/print/deliveryslip.php
@@ -3,53 +3,55 @@
 /*
 Array
 (
-    [id] => 1
-    [ordertime] => 0000-00-00 00:00:00
-    [assigntime] => 2011-12-12 10:02:41
-    [deliverytime] => 0000-00-00 00:00:00
-    [assignment_date] => 2011-12-14
-    [delivery_id] => 00000017-10-122011-0000000001
-    [application_id] => 1
-    [application_key] => 23c33397a9b1ecb579c53fe200e26c12709ee379
-    [buyer_id] => 1
-    [merchant_id] => 17
-    [merchant_trans_id] => 123456789
-    [courier_id] => 0
-    [device_id] => 16
-    [shipping_address] => Kompleks DKI D3 Joglo
-    [phone] => 02112345678
-    [status] => assigned
-    [delivery_note] => 
-    [undersign] => 
-    [latitude] => 0.000000000000
-    [longitude] => 0.000000000000
-    [reschedule_ref] => 
-    [revoke_ref] => 
+	[id] => 1
+	[ordertime] => 0000-00-00 00:00:00
+	[assigntime] => 2011-12-12 10:02:41
+	[deliverytime] => 0000-00-00 00:00:00
+	[assignment_date] => 2011-12-14
+	[delivery_id] => 00000017-10-122011-0000000001
+	[application_id] => 1
+	[application_key] => 23c33397a9b1ecb579c53fe200e26c12709ee379
+	[buyer_id] => 1
+	[merchant_id] => 17
+	[merchant_trans_id] => 123456789
+	[courier_id] => 0
+	[device_id] => 16
+	[shipping_address] => Kompleks DKI D3 Joglo
+	[phone] => 02112345678
+	[status] => assigned
+	[delivery_note] =>
+	[undersign] =>
+	[latitude] => 0.000000000000
+	[longitude] => 0.000000000000
+	[reschedule_ref] =>
+	[revoke_ref] =>
 )
 
 */
 ?>
 	<style>
-		
+
 		#mainInfo{
 			width:500px;
 		}
-		
+
 		.dataTable{
 			width:100%;
 			font-family:'Trebuchet Ms', 'Yanone Kaffeesatz', Lato, Lobster, 'Lobster Two','Droid Sans', Arial ;
+			font-size:12px;
+
 		}
-		
+
 		#mainInfo td{
 			border:thin solid #eee;
 		}
-		
+
 		h1{
 			border-bottom:thin solid #eee;
 			text-align:center;
 			font-family:'Trebuchet Ms', 'Yanone Kaffeesatz', Lato, Lobster, 'Lobster Two','Droid Sans', Arial ;
 		}
-		
+
 		.dataTable td{
 			border:thin solid #eee;
 		}
@@ -57,11 +59,13 @@ Array
 		#signBox td{
 			border:thin solid #eee;
 		}
-		
+
 		table#signBox {
 			border:thin solid #eee;
-			width:500px;
+			width:100%;
 			font-family:'Trebuchet Ms', 'Yanone Kaffeesatz', Lato, Lobster, 'Lobster Two','Droid Sans', Arial ;
+			font-size:12px;
+			margin-bottom:35px;
 		}
 
 		th{
@@ -69,41 +73,83 @@ Array
 			background-color:black;
 			color:white;
 		}
-		
+
 		#signBox{
 			float:right;
 		}
-		
+
 		#wrapper{
 			width:1000px;
 			display:block;
 			font-family:'Trebuchet Ms', 'Yanone Kaffeesatz', Lato, Lobster, 'Lobster Two','Droid Sans', Arial ;
 		}
-		
+
 	</style>
 <div id="wrapper">
 
-	<h1>Delivery Slip</h1>
-	
-	<table border="0" cellpadding="4" cellspacing="0" id="mainInfo">
-	<tbody>
-	<tr>
-		<td>Delivery Number:</td>
-		<td><?php print $main_info['delivery_id'];?></td>
-	</tr>
-	<tr>
-		<td>Merchant Transaction ID:</td>
-		<td><?php print $main_info['merchant_trans_id'];?></td>
-	</tr>
-	<tr>
-		<td>Shipping Address:</td>
-		<td><?php print $main_info['shipping_address'];?></td>
-	</tr>
-	<tr>
-		<td>Contact Number:</td>
-		<td><?php print $main_info['phone'];?></td>
-	</tr>
-	</tbody>
+	<h1>DELIVERY NOTE</h1>
+	<table>
+		<tr>
+			<td>
+				LOGO
+			</td>
+			<td>
+				<table border="0" cellpadding="4" cellspacing="0" id="mainInfo">
+					<tbody>
+						<tr>
+							<td>Delivery Number:</td>
+							<td><?php print $main_info['delivery_id'];?></td>
+						</tr>
+						<tr>
+							<td>Delivery Date:</td>
+							<td><?php print $main_info['assignment_date'];?></td>
+						</tr>
+						<tr>
+							<td>Delivery Slot:</td>
+							<td><?php print $main_info['assignment_timeslot'];?></td>
+						</tr>
+					</tbody>
+				</table>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<table border="0" cellpadding="4" cellspacing="0" id="mainInfo">
+					<tbody>
+						<tr>
+							<td>Online Store:</td>
+							<td><?php print $main_info['merchant_id'];?></td>
+						</tr>
+						<tr>
+							<td>Transaction ID:</td>
+							<td><?php print $main_info['merchant_trans_id'];?></td>
+						</tr>
+						<tr>
+							<td>Store Detail:</td>
+							<td>&nbsp;</td>
+						</tr>
+					</tbody>
+				</table>
+			</td>
+			<td>
+				<table border="0" cellpadding="4" cellspacing="0" id="mainInfo">
+					<tbody>
+						<tr>
+							<td>Delivered To:</td>
+							<td><?php print $main_info['recipient_name'];?></td>
+						</tr>
+						<tr>
+							<td>Shipping Address:</td>
+							<td><?php print $main_info['shipping_address'];?></td>
+						</tr>
+						<tr>
+							<td>Contact Number:</td>
+							<td><?php print $main_info['phone'];?></td>
+						</tr>
+					</tbody>
+				</table>
+			</td>
+		</tr>
 	</table>
 	<br /><br />
 
@@ -112,21 +158,39 @@ Array
 	<br /><br />
 
 	<table border="0" cellpadding="4" cellspacing="0" id="signBox">
-	<thead>
-		<tr>
-			<th>Delivered By:</th>
-			<th>Received By:</th>
-		</tr>
-	</thead>
-	<tbody>
-	<tr style="height:150px;">
-		<td>&nbsp;</td>
-		<td>&nbsp;</td>
-	</tr>
-	<tr>
-		<td>Date:</td>
-		<td>Date:</td>
-	</tr>
-	</tbody>
+		<thead>
+			<tr>
+				<th>Created By</th>
+				<th>Online Store</th>
+				<th>Goods Received By</th>
+				<th>Cash Received By</th>
+				<th>Reporting</th>
+				<th>Staff Dispatch Admin</th>
+				<th>Finance</th>
+				<th>Courier</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr style="height:150px;">
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+		</tbody>
 	</table>
 </div>
