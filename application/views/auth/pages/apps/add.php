5d89d563d8e710efa4984215106b036df1d38f54
Andy Awidarto <andy@sinaptix.com>
2012-02-23 20:13:40 +0700

    css for fieldset

--- application/views/auth/pages/apps/add.php
+++ application/views/auth/pages/apps/add.php
@@ -6,14 +6,14 @@
 			Merchant:<br />
 			<strong><?php echo $merchant_name;?></strong><br /><br />
 			-->
+
+			<?php print form_fieldset('Application Info'); ?>
+			
 			Application Name:<br />
 			<input type="text" name="application_name" size="50" class="form" value="<?php echo set_value('application_name'); ?>" /><br /><?php echo form_error('application_name'); ?><br />
 
 			Application Domain:<br />
 			<input type="text" name="domain" size="50" class="form" value="<?php echo set_value('domain'); ?>" /><br /><?php echo form_error('domain'); ?><br />
-
-			Logo URL:<br />
-			<input type="text" name="logo_url" size="50" class="form" value="<?php echo set_value('logo_url'); ?>" /><br /><?php echo form_error('logo_url'); ?><br />
 
 			Callback URL:<br />
 			<input type="text" name="callback_url" size="50" class="form" value="<?php echo set_value('callback_url'); ?>" /><br /><?php echo form_error('callback_url'); ?><br />
@@ -27,8 +27,16 @@
 			Application Description:<br />
 			<textarea name="application_description" cols="60" rows="10"><?php echo set_value('application_description'); ?></textarea><br />
 
+			<?php print form_fieldset_close(); ?>
+
+			<?php print form_fieldset('Email Customization'); ?>
+			Logo URL:<br />
+			<input type="text" name="logo_url" size="50" class="form" value="<?php echo set_value('logo_url'); ?>" /><br /><?php echo form_error('logo_url'); ?><br />
+
 			Signature:<br />
 			<textarea name="signature" cols="60" rows="10"><?php echo set_value('signature'); ?></textarea><br />
+
+			<?php print form_fieldset_close(); ?>
 
 			<input type="submit" value="Add" name="add" />
 				<?php
--- application/views/auth/pages/apps/edit.php
+++ application/views/auth/pages/apps/edit.php
@@ -2,14 +2,14 @@
 	<div id="form_box">
 			<form method="post" action="<?php echo site_url($act_url)?>">
 			<input type="hidden" name="merchant_id" value="<?php echo $merchant_id ?>" />
+
+			<?php print form_fieldset('Application Info'); ?>
+
 			Application Name:<br />
 			<input type="text" name="application_name" size="50" class="form" value="<?php echo set_value('application_name',$user['application_name']); ?>" /><br /><?php echo form_error('application_name'); ?><br />
 
 			Application Domain:<br />
 			<input type="text" name="domain" size="50" class="form" value="<?php echo set_value('domain',$user['domain']); ?>" /><br /><?php echo form_error('domain'); ?><br />
-
-			Logo URL:<br />
-			<input type="text" name="logo_url" size="50" class="form" value="<?php echo set_value('logo_url',$user['logo_url']); ?>" /><br /><?php echo form_error('logo_url'); ?><br />
 
 			Callback URL:<br />
 			<input type="text" name="callback_url" size="50" class="form" value="<?php echo set_value('callback_url',$user['callback_url']); ?>" /><br /><?php echo form_error('callback_url'); ?><br />
@@ -23,8 +23,17 @@
 			Application Description:<br />
 			<textarea name="application_description" cols="60" rows="10"><?php echo set_value('application_description',$user['application_description']); ?></textarea><br />
 
+			<?php print form_fieldset_close(); ?>
+
+			<?php print form_fieldset('Email Customization'); ?>
+
+			Logo URL:<br />
+			<input type="text" name="logo_url" size="50" class="form" value="<?php echo set_value('logo_url',$user['logo_url']); ?>" /><br /><?php echo form_error('logo_url'); ?><br />
+
 			Signature:<br />
 			<textarea name="signature" cols="60" rows="10"><?php echo set_value('signature',$user['signature']); ?></textarea><br />
+
+			<?php print form_fieldset_close(); ?>
 
 			<input type="submit" value="Edit" name="edit" />
 				<?php
