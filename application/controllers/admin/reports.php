db03bd42555cce188feb5ee6b0d154753cba4be3
Andy Awidarto <andy@sinaptix.com>
2012-07-23 17:17:39 +0700

    reconciliation section

--- application/controllers/admin/reports.php
+++ application/controllers/admin/reports.php
@@ -65,13 +65,13 @@ class Reports extends Application
 			'Generate'
 			); // Setting headings for the table
 
-		$page['ajaxurl'] = 'admin/reports/ajaxreconsiliation';
+		$page['ajaxurl'] = 'admin/reports/ajaxreconciliation';
 		$page['page_title'] = 'Reconciliations';
-		$this->ag_auth->view('ajaxlistview',$page); // Load the view
+		$this->ag_auth->view('reconajaxlistview',$page); // Load the view
 		
 	}
 
-	public function ajaxreconsiliation(){
+	public function ajaxreconciliation($type = null){
 
 
 		$week = date('W',time());
@@ -85,6 +85,9 @@ class Reports extends Application
 			$from =	date('d-m-Y', strtotime('1 Jan '.$year.' +'.($i - 1).' weeks'));
 			$to = date('d-m-Y', strtotime('1 Jan '.$year.' +'.$i.' weeks - 1 day'));
 
+			$printrecon = '<span class="printslip" id="'.$from.'_'.$to.'" style="cursor:pointer;text-decoration:underline;" >Global</span>';
+
+
 			$generate = anchor("admin/reports/globalreport/".$from."/".$to, "Global"); // Build actions links
 			$merchantlist = anchor("admin/reports/merchants/".$from."/".$to, "By Merchant"); // Build actions links
 			$courierlist = anchor("admin/reports/couriers/".$from."/".$to, "By Courier"); // Build actions links
@@ -93,7 +96,7 @@ class Reports extends Application
 				$i,
 				date('d-m-Y', strtotime('1 Jan '.$year.' +'.($i - 1).' weeks')),
 				date('d-m-Y', strtotime('1 Jan '.$year.' +'.$i.' weeks - 1 day')),
-				$generate.' '.$merchantlist.' '.$courierlist
+				$printrecon.' '.$merchantlist.' '.$courierlist
 			); // Adding row to table
 		}
 
@@ -113,27 +116,256 @@ class Reports extends Application
 
 	public function merchants($from,$to){
 		$this->breadcrumb->add_crumb('Reconciliations','admin/reports/reconciliation');
+		$this->breadcrumb->add_crumb('Merchants '.$from.' - '.$to,'admin/reports/merchants');
 
 		$this->table->set_heading(
-			'Year',
-			'Week',
-			'From',
-			'To',
-			'Generate'
+			 	'Merchant Name',
+			 	'Email',
+			 	'Full Name',
+			 	'Mobile',
+			 	'Phone',
+			 	'Joined Since',
+			 	'Action'
 			); // Setting headings for the table
 
 		$page['ajaxurl'] = 'admin/reports/ajaxmerchants/'.$from.'/'.$to;
-		$page['page_title'] = 'Merchant Reconciliations';
-		$this->ag_auth->view('ajaxlistview',$page); // Load the view
+		$page['page_title'] = 'Merchants Reconciliations';
+		$this->ag_auth->view('reconajaxlistview',$page); // Load the view
 
 	}
 
 	public function ajaxmerchants($from,$to){
 
+		$limit_count = $this->input->post('iDisplayLength');
+		$limit_offset = $this->input->post('iDisplayStart');
+
+		$sort_col = $this->input->post('iSortCol_0');
+		$sort_dir = $this->input->post('sSortDir_0');
+
+		$group_id = user_group_id('merchant');
+
+		$columns = array(
+			'email',
+			//'password',
+			//'merchantname',
+			'fullname',
+			'street',
+			'district',
+			'city',
+			'province',
+			'country',
+			'zip',
+			'mobile',
+			'phone',
+			'created',
+			'groupname',
+			'bank',
+			'account_number',
+			'account_name',
+			'group_id',
+			'token',
+			'identifier',
+			'merchant_request',
+			'success',
+			'fail'
+		);
+
+		// get total count result
+		$count_all = $this->db->count_all($this->config->item('jayon_members_table'));
+
+		$count_display_all = $this->db->count_all_results($this->config->item('jayon_members_table'));
+
+		$this->db->select('*,g.description as groupname');
+		$this->db->join('groups as g','members.group_id = g.id','left');
+
+		$search = false;
+				//search column
+		if($this->input->post('sSearch') != ''){
+			$srch = $this->input->post('sSearch');
+			//$this->db->like('buyerdeliveryzone',$srch);
+			$this->db->or_like('buyerdeliverytime',$srch);
+			$this->db->or_like('delivery_id',$srch);
+			$search = true;
+		}
+
+		if($this->input->post('sSearch_0') != ''){
+			$this->db->like('username',$this->input->post('sSearch_0'));
+			$search = true;
+		}
+
+
+		if($this->input->post('sSearch_1') != ''){
+			$this->db->like('email',$this->input->post('sSearch_1'));
+			$search = true;
+		}
+
+		if($this->input->post('sSearch_2') != ''){
+			$this->db->like('fullname',$this->input->post('sSearch_2'));
+			$search = true;
+		}
+
+		if($this->input->post('sSearch_3') != ''){
+			$this->db->like('street',$this->input->post('sSearch_3'));
+			$search = true;
+		}
+
+		if($this->input->post('sSearch_4') != ''){
+			$this->db->like('district',$this->input->post('sSearch_4'));
+			$search = true;
+		}
+
+		if($this->input->post('sSearch_5') != ''){
+			$this->db->like('city',$this->input->post('sSearch_5'));
+			$search = true;
+		}
+		if($this->input->post('sSearch_6') != ''){
+			$this->db->like('province',$this->input->post('sSearch_6'));
+			$search = true;
+		}
+
+		if($this->input->post('sSearch_7') != ''){
+			$this->db->like('country',$this->input->post('sSearch_7'));
+			$search = true;
+		}
+
+		if($this->input->post('sSearch_8') != ''){
+			$this->db->like('zip',$this->input->post('sSearch_8'));
+			$search = true;
+		}
+
+		if($this->input->post('sSearch_9') != ''){
+			$this->db->like('mobile',$this->input->post('sSearch_9'));
+			$search = true;
+		}
+
+		if($this->input->post('sSearch_10') != ''){
+			$this->db->like('phone',$this->input->post('sSearch_10'));
+			$search = true;
+		}
+
+		if($this->input->post('sSearch_11') != ''){
+			$this->db->like('created',$this->input->post('sSearch_11'));
+			$search = true;
+		}
+
+		if($search){
+			//$this->db->and_();
+		}		
+
+
+		$data = $this->db
+			->where('group_id',$group_id)
+			->where('created < ', $from)
+			->limit($limit_count, $limit_offset)
+			->order_by($columns[$sort_col],$sort_dir)
+			->get($this->config->item('jayon_members_table'));
+
+		//print $this->db->last_query();
+
+		$result = $data->result_array();
+
+		$aadata = array();
+
+
+		foreach($result as $value => $key)
+		{
+			$delete = anchor("admin/members/delete/".$key['id']."/", "Delete"); // Build actions links
+			$editpass = anchor("admin/members/editpass/".$key['id']."/", "Password"); // Build actions links
+			if($key['group_id'] === group_id('merchant')){
+				$addapp = anchor("admin/members/merchantmanage/".$key['id']."/", "Applications"); // Build actions links
+			}else{
+				$addapp = '&nbsp'; // Build actions links
+			}
+			$edit = anchor("admin/members/edit/".$key['id']."/", "Edit"); // Build actions links
+			$detail = form_checkbox('assign[]',$key['id'],FALSE,'class="assign_check"').' '.anchor("admin/members/details/".$key['id']."/", $key['username']); // Build detail links
+
+			$printrecon = '<span class="printslip" id="'.$key['id'].'" style="cursor:pointer;text-decoration:underline;" >View</span>';
+			
+			$aadata[] = array(
+			 	$key['merchantname'],
+			 	$key['email'],
+			 	$key['fullname'],
+			 	$key['mobile'],
+			 	$key['phone'],
+			 	$key['created'],
+			 	$printrecon
+			); // Adding row to table
+
+		}
+
+		$result = array(
+			'sEcho'=> $this->input->post('sEcho'),
+			'iTotalRecords'=>$count_all,
+			'iTotalDisplayRecords'=> $count_display_all,
+			'aaData'=>$aadata
+		);
+
+		print json_encode($result);
+
 	}
 
 	public function couriers($from,$to){
 		$this->breadcrumb->add_crumb('Reconciliations','admin/reports/reconciliation');
+		$this->breadcrumb->add_crumb('Couriers '.$from.' - '.$to,'admin/reports/couriers');
+
+		$this->table->set_heading('Username', 'Email','Full Name','Mobile','Phone','Actions'); // Setting headings for the table
+
+		$page['ajaxurl'] = 'admin/reports/ajaxcouriers/'.$from.'/'.$to;
+		$page['page_title'] = 'Couriers Reconciliations';
+		$this->ag_auth->view('reconajaxlistview',$page); // Load the view
+	}
+
+	public function ajaxcouriers($from,$to){
+
+		$limit_count = $this->input->post('iDisplayLength');
+		$limit_offset = $this->input->post('iDisplayStart');
+		
+		$sort_col = $this->input->post('iSortCol_0');
+		$sort_dir = $this->input->post('sSortDir_0');
+
+		$columns = array(
+			'username','email','fullname','mobile','phone'
+			);
+
+		// get total count result
+		$count_all = $this->db->count_all($this->config->item('jayon_couriers_table'));
+
+		$count_display_all = $this->db->count_all_results($this->config->item('jayon_couriers_table'));
+		
+		$data = $this->db->limit($limit_count, $limit_offset)->order_by($columns[$sort_col],$sort_dir)->get($this->config->item('jayon_couriers_table'));
+		
+		//print $this->db->last_query();
+		
+		$result = $data->result_array();
+			
+		$aadata = array();
+		
+		
+		foreach($result as $value => $key)
+		{
+			$delete = anchor("admin/couriers/delete/".$key['id']."/", "Delete"); // Build actions links
+			$editpass = anchor("admin/couriers/editpass/".$key['id']."/", "Change Password"); // Build actions links
+			$edit = anchor("admin/couriers/edit/".$key['id']."/", "Edit"); // Build actions links
+			$detail = anchor("admin/couriers/details/".$key['id']."/", $key['username']); // Build detail links
+
+			$printrecon = '<span class="printslip" id="'.$key['id'].'" style="cursor:pointer;text-decoration:underline;" >View</span>';
+
+			$aadata[] = array($detail, $key['email'],$key['fullname'],$key['mobile'],$key['phone'],$printrecon); // Adding row to table
+		}
+		
+		$result = array(
+			'sEcho'=> $this->input->post('sEcho'),
+			'iTotalRecords'=>$count_all,
+			'iTotalDisplayRecords'=> $count_display_all,
+			'aaData'=>$aadata
+		);
+		
+		print json_encode($result);
+	}
+
+	public function globalreport($from,$to){
+		$this->breadcrumb->add_crumb('Reconciliations','admin/reports/reconciliation');
+		$this->breadcrumb->add_crumb('Global '.$from.' - '.$to,'admin/reports/globalreport');
 
 		$this->table->set_heading(
 			'Year',
@@ -143,17 +375,9 @@ class Reports extends Application
 			'Generate'
 			); // Setting headings for the table
 
-		$page['ajaxurl'] = 'admin/reports/ajaxcouriers/'.$from.'/'.$to;
-		$page['page_title'] = 'Reconciliations';
-		$this->ag_auth->view('ajaxlistview',$page); // Load the view
-	}
-
-	public function ajaxcouriers($id){
-
-	}
-
-	public function globalreport($from,$to){
-
+		$page['ajaxurl'] = 'admin/reports/ajaxglobal/'.$from.'/'.$to;
+		$page['page_title'] = 'Global Reconciliations';
+		$this->ag_auth->view('reconajaxlistview',$page); // Load the view
 	}
 
 }
---
+++ application/views/auth/pages/reconajaxlistview.php
@@ -0,0 +1,232 @@
+<script>
+	var asInitVals = new Array();
+
+	$(document).ready(function() {
+
+	    var oTable = $('.dataTable').dataTable(
+			{
+				"bProcessing": true,
+		        "bServerSide": true,
+		        "sAjaxSource": "<?php print site_url($ajaxurl);?>",
+				"oLanguage": { "sSearch": "Search "},
+			<?php if($this->config->item('infinite_scroll')):?>
+				"bScrollInfinite": true,
+			    "bScrollCollapse": true,
+			    "sScrollY": "500px",
+			<?php endif; ?>
+			<?php if(isset($sortdisable)):?>
+				"aoColumnDefs": [ 
+				    { "bSortable": false, "aTargets": [ <?php print $sortdisable; ?> ] }
+				 ],
+			<?php endif;?>
+			    "fnServerData": function ( sSource, aoData, fnCallback ) {
+		            $.ajax( {
+		                "dataType": 'json', 
+		                "type": "POST", 
+		                "url": sSource, 
+		                "data": aoData, 
+		                "success": fnCallback
+		            } );
+		        }
+			}
+		);
+
+		$('tfoot input').keyup( function () {
+			/* Filter on the column (the index) of this element */
+			oTable.fnFilter( this.value, $('tfoot input').index(this) );
+		} );
+
+		/*
+		 * Support functions to provide a little bit of 'user friendlyness' to the textboxes in 
+		 * the footer
+		 */
+		$('tfoot input').each( function (i) {
+			asInitVals[i] = this.value;
+		} );
+
+		$('tfoot input').focus( function () {
+			if ( this.className == 'search_init' )
+			{
+				this.className = '';
+				this.value = '';
+			}
+		} );
+
+		$('tfoot input').blur( function (i) {
+			if ( this.value == '' )
+			{
+				this.className = 'search_init';
+				this.value = asInitVals[$('tfoot input').index(this)];
+			}
+		} );
+
+		$('table.dataTable').click(function(e){
+			if ($(e.target).is('.cancel_link')) {
+				var delivery_id = e.target.id;
+				var answer = confirm("Are you sure you want to archive this order ?");
+				if (answer){
+					$.post('<?php print site_url('admin/delivery/ajaxarchive');?>',{'delivery_id':delivery_id}, function(data) {
+						if(data.result == 'ok'){
+							//redraw table
+							oTable.fnDraw();
+							alert(delivery_id + " archived");
+						}
+					},'json');
+				}else{
+					alert(delivery_id + " not archived");
+				}
+		   	}
+
+			if ($(e.target).is('.printslip')) {
+				var delivery_id = e.target.id;
+				$('#print_id').val(delivery_id);
+				var src = '<?php print base_url() ?>admin/prints/deliveryslip/' + delivery_id;
+
+				$('#print_frame').attr('src',src);
+				$('#print_dialog').dialog('open');
+			}
+
+			if ($(e.target).is('.view_detail')) {
+				var delivery_id = e.target.id;
+				var src = '<?php print base_url() ?>admin/prints/deliveryview/' + delivery_id;
+
+				$('#view_frame').attr('src',src);
+				$('#view_dialog').dialog('open');
+			}
+		});
+
+		$('#doArchive').click(function(){
+			var assigns = '';
+			var count = 0;
+			$('.assign_check:checked').each(function(){
+
+				var deliverydate = $('#dt_'+this.value).html();
+				var status = $('#st_'+this.value).html();
+				assigns += '<li style="padding:5px;border-bottom:thin solid grey;margin-left:0px;"><strong>'+this.value + '</strong><br />' + deliverydate +' '+ status+'</li>';
+				count++;
+			});
+			
+			if(count > 0){
+				$('#archive_list').html(assigns);
+				$('#archive_dialog').dialog('open');
+			}else{
+				alert('Please select one or more delivery orders');
+			}
+		});
+
+		$('#archive_dialog').dialog({
+			autoOpen: false,
+			height: 300,
+			width: 400,
+			modal: true,
+			buttons: {
+				"Archive Delivery Orders": function() {
+					var delivery_ids = [];
+					var laststatus = [];
+					i = 0;
+					$('.assign_check:checked').each(function(){
+						delivery_ids[i] = $(this).val();
+						laststatus[i] = $(this).attr('title');
+						i++;
+					}); 
+					$.post('<?php print site_url('admin/delivery/ajaxarchive');?>',{ assignment_date: $('#assign_deliverytime').val(),'delivery_id[]':delivery_ids,'laststatus[]':laststatus}, function(data) {
+						if(data.result == 'ok'){
+							//redraw table
+							oTable.fnDraw();
+							$('#archive_dialog').dialog( "close" );
+						}
+					},'json');
+				},
+				Cancel: function() {
+					$( this ).dialog( "close" );
+				}
+			},
+			close: function() {
+				//allFields.val( "" ).removeClass( "ui-state-error" );
+				$('#archive_list').html('');
+			}
+		});
+
+		$('#print_dialog').dialog({
+			autoOpen: false,
+			height: 700,
+			width: 1050,
+			modal: true,
+			buttons: {
+				Print: function(){
+					var pframe = document.getElementById('print_frame');
+					var pframeWindow = pframe.contentWindow;
+					pframeWindow.print();
+				}, 
+				"Download PDF": function(){
+					var print_id = $('#print_id').val();
+					var src = '<?php print base_url() ?>admin/prints/deliveryslip/' + print_id + '/pdf';
+					window.location = src;
+					//alert(src);
+				},
+				Close: function() {
+					$( this ).dialog( "close" );
+				}
+			},
+			close: function() {
+				
+			}
+		});
+
+		$('#view_dialog').dialog({
+			autoOpen: false,
+			height: 600,
+			width: 900,
+			modal: true,
+			buttons: {
+				Print: function(){
+					var pframe = document.getElementById('print_frame');
+					var pframeWindow = pframe.contentWindow;
+					pframeWindow.print();
+				}, 
+				Close: function() {
+					oTable.fnDraw();
+					$( this ).dialog( "close" );
+				}
+			},
+			close: function() {
+				
+			}
+		});
+	});
+</script>
+<?php if(isset($add_button)):?>
+	<div class="button_nav">
+		<?php echo anchor($add_button['link'],$add_button['label'],'class="button add"')?>
+	</div>
+<?php endif;?>
+<?php echo $this->table->generate(); ?>
+
+<div id="archive_dialog" title="Archive Delivery Orders">
+	<table style="width:100%;border:0;margin:0;">
+		<tr>
+			<td style="width:250px;vertical-align:top">
+				Delivery Orders :
+			</td>
+		</tr>
+		<tr>
+			<td style="overflow:auto;width:250px;vertical-align:top">
+				<ul id="archive_list" style="border-top:thin solid grey;list-style-type:none;padding-left:0px;"></ul>
+			</td>
+		</tr>
+	</table>
+</div>
+
+<div id="print_dialog" title="Print" style="overflow:hidden;padding:8px;">
+	<input type="hidden" value="" id="print_id" />
+	<iframe id="print_frame" name="print_frame" width="100%" height="100%"
+    marginWidth="0" marginHeight="0" frameBorder="0" scrolling="auto"
+    title="Dialog Title">Your browser does not suppr</iframe>
+</div>
+
+<div id="view_dialog" title="Order Detail" style="overflow:hidden;padding:8px;">
+	<input type="hidden" value="" id="print_id" />
+	<iframe id="view_frame" name="print_frame" width="100%" height="100%"
+    marginWidth="0" marginHeight="0" frameBorder="0" scrolling="auto"
+    title="Dialog Title">Your browser does not suppr</iframe>
+</div>
