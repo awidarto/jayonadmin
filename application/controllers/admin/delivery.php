cc88db64531b0d8dbb93da384080ae7f4699169f
Andy Awidarto <andy.awidarto@gmail.com>
2014-05-20 23:56:29 +0700

    incoming search error

--- application/controllers/admin/delivery.php
+++ application/controllers/admin/delivery.php
@@ -238,13 +238,15 @@ class Delivery extends Application
 		}
 
 
-        $this->db->where($this->config->item('incoming_delivery_table').'.pending_count < ',1);
-        $this->db->and_();
 		$this->db->group_start()
 			->where($this->config->item('incoming_delivery_table').'.status',$this->config->item('trans_status_new'))
 			->or_where($this->config->item('incoming_delivery_table').'.status',$this->config->item('trans_status_confirmed'))
 			->not_like($this->config->item('incoming_delivery_table').'.status','assigned','before')
 			->group_end();
+        /*
+        $this->db->and_();
+        $this->db->where($this->config->item('incoming_delivery_table').'.pending_count < ',1);
+        */
 
         $dbca = clone $this->db;
 
