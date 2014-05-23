e0f2f144c13fd18dd7defd9cf09de546d0946340
Andy Awidarto <andy.awidarto@gmail.com>
2014-05-23 23:01:24 +0700

    refresh

--- application/controllers/admin/delivery.php
+++ application/controllers/admin/delivery.php
@@ -239,14 +239,11 @@ class Delivery extends Application
 
 
 		$this->db->group_start()
+            ->where($this->config->item('incoming_delivery_table').'.pending_count < ',1)
 			->where($this->config->item('incoming_delivery_table').'.status',$this->config->item('trans_status_new'))
 			->or_where($this->config->item('incoming_delivery_table').'.status',$this->config->item('trans_status_confirmed'))
 			->not_like($this->config->item('incoming_delivery_table').'.status','assigned','before')
 			->group_end();
-        /*
-        $this->db->and_();
-        $this->db->where($this->config->item('incoming_delivery_table').'.pending_count < ',1);
-        */
 
         $dbca = clone $this->db;
 
