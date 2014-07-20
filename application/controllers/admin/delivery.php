e4c3a52c3e8f04c5e35018f65be3a00bacd6306d
Andy Awidarto <andy.awidarto@gmail.com>
2014-07-20 17:24:36 +0700

    cancel page fix

--- application/controllers/admin/delivery.php
+++ application/controllers/admin/delivery.php
@@ -1267,6 +1267,25 @@ class Delivery extends Application
 			form_button('do_cancel','Cancel Selection','id="doCancel"')
 			);
 
+        $pd = get_print_default();
+
+        if($pd){
+            $page['resolution'] = $pd['res'];
+            $page['cell_width'] = $pd['cell_width'];
+            $page['cell_height'] = $pd['cell_height'];
+            $page['columns'] = $pd['col'];
+            $page['margin_right'] = $pd['mright'];
+            $page['margin_bottom'] = $pd['mbottom'];
+        }else{
+            $page['resolution'] = 150;
+            $page['cell_width'] = 450;
+            $page['cell_height'] = 250;
+            $page['columns'] = 2;
+            $page['margin_right'] = 10;
+            $page['margin_bottom'] = 10;
+        }
+
+
 		$page['sortdisable'] = '0,2';
 		$page['ajaxurl'] = 'admin/delivery/ajaxcanceled';
 		$page['page_title'] = 'Canceled Delivery Orders';
