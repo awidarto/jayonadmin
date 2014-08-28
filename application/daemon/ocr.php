a3aad3ebc9c047aa20c9e44ba79e0f40ed4407bc
Andy Awidarto <andy.awidarto@gmail.com>
2014-08-29 00:13:26 +0700

    ocr

--- application/daemon/ocr.php
+++ application/daemon/ocr.php
@@ -1,10 +1,11 @@
 <?php
 require_once('../libraries/TesseractOCR.php');
-$files = glob( realpath('../../public/pickup/*_address.jpg') );
+$files = glob( '../../public/pickup/*_address.jpg' );
 
 foreach($files as $file){
     print($file);
     $tesseract = new TesseractOCR('images/some-words.jpg');
+    $tesseract->setTempDir(realpath('temp'));
     echo $tesseract->recognize();
 }
 
