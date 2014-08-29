123c0e10c3af8fdba6f7f6e153fa1aec209e9113
Andy Awidarto <andy.awidarto@gmail.com>
2014-08-29 09:31:26 +0700

    ocr

--- application/daemon/ocr.php
+++ application/daemon/ocr.php
@@ -4,13 +4,13 @@ require_once('/var/www/beta2/jayonadmin/application/libraries/TesseractOCR.php')
 $files = glob( '/var/www/beta2/jayonadmin/public/pickup/*_address.jpg' );
 
 foreach($files as $file){
-    print($file);
+
     $tesseract = new TesseractOCR($file);
     $tesseract->setTempDir(realpath('temp'));
     $savefile = str_replace('pickup', 'ocr', $file);
     $savefile = str_replace('.jpg', '.txt', $savefile);
     if(file_exists($savefile)){
-
+        print $savefile." exists, skipping\r\n";
     }else{
         $result = $tesseract->recognize();
         file_put_contents($savefile, $result);
