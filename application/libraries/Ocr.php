d6ab651cdb0ab93d2d5cb4208170b7110f0c5554
Andy Awidarto <andy.awidarto@gmail.com>
2014-08-29 09:05:23 +0700

    ocr ajax

--- application/libraries/Ocr.php
+++ application/libraries/Ocr.php
@@ -8,7 +8,7 @@ class Ocr{
 
     public function __construct($file){
         $this->ocrinstance = new TesseractOCR($file);
-        $this->ocrinstance->setTempDir( realpath(APPPATH.'daemon/temp')  );
+        $this->ocrinstance->setTempDir( '/var/www/beta2/jayonadmin/'.APPPATH.'daemon/temp' );
     }
 
     public function execute(){
