0aceac8fd0451a0e4282f654f5becc6048cda22f
Andy Awidarto <andy.awidarto@gmail.com>
2014-11-21 15:35:56 +0700

    delivered export

--- application/controllers/admin/dl.php
+++ application/controllers/admin/dl.php
@@ -435,6 +435,7 @@ class Dl extends Application
             ->or_where($this->config->item('assigned_delivery_table').'.status',$this->config->item('trans_status_mobile_noshow'))
             ->or_where($this->config->item('assigned_delivery_table').'.status',$this->config->item('trans_status_mobile_return'))
             ->group_end();
+
         $data = $this->db->order_by('deliverytime','desc')
                     ->get($this->config->item('assigned_delivery_table'));
 
