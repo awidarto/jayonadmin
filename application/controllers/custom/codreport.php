0a3930527252d50c9e723754d9bfa8fdc8868c8c
Andy Awidarto <andy.awidarto@gmail.com>
2015-06-05 11:08:47 +0700

    checkbox

--- application/controllers/custom/codreport.php
+++ application/controllers/custom/codreport.php
@@ -1624,6 +1624,7 @@ class Codreport extends Application
 
                     if($cells[$i][$j] == '_table_'){
                         $splice_at = $i;
+                        $cells[$i][$j] = '';
                     }
                 }
             }
